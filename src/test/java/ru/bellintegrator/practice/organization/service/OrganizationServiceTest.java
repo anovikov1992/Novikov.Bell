5f44f404a1f053abd6611904dc18469cee6b33b5
anovikov1992 <anovikov1992@yandex.ru>
2018-08-11 13:38:40 +0400

    tests

--- src/test/java/ru/bellintegrator/practice/organization/service/OrganizationServiceTest.java
+++ src/test/java/ru/bellintegrator/practice/organization/service/OrganizationServiceTest.java
@@ -10,24 +10,35 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
 import ru.bellintegrator.practice.Application;
 import ru.bellintegrator.practice.office.dao.OfficeDao;
+import ru.bellintegrator.practice.organization.MyException.OrgOutException;
+import ru.bellintegrator.practice.organization.MyException.OrganisationValidationException;
 import ru.bellintegrator.practice.organization.dao.OrganizationDao;
 import ru.bellintegrator.practice.organization.model.Organization;
 import ru.bellintegrator.practice.organization.view.OrganizationView;
+import ru.bellintegrator.practice.organization.view.OrganizationViewList;
+import ru.bellintegrator.practice.organization.view.OrganizationViewSave;
+import ru.bellintegrator.practice.organization.view.OrganizationViewUpdate;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @RunWith(SpringRunner.class)
 public class OrganizationServiceTest {
 
-
     private Organization organization;
+
+
     @TestConfiguration
     static class OrganizationServiceImplTestContextConfiguration {
 
@@ -45,14 +56,11 @@ public class OrganizationServiceTest {
     @Mock
     private OrganizationDao organizationDao;
 
-    @Mock
-    private OfficeDao officeDao;
 
     @Before
     public void setup() {
 
         organization = new Organization();
-
         organization.setId(1L);
         organization.setName("ООО Test");
         organization.setFullName("Общество с ограниченной ответственностью Test");
@@ -60,19 +68,120 @@ public class OrganizationServiceTest {
         organization.setKpp(123456897L);
         organization.setPhone(3216589665L);
         organization.setActive(true);
+    }
 
-        when(organizationDao.loadById(1L)).thenReturn(organization);
+    @Test
+    public void verifyLoadOrganizationByName() throws Exception {
 
-//        when(organizationDao.getAllOrganization()).thenReturn();
+        when(organizationDao.getOrganizationByName("ООО Test", null,null)).thenReturn(organization);
+        OrganizationViewList view = organizationService.getOrganizationByName("ООО Test", null, null);
+        assertEquals(organization.getName(), view.name);
+
+    }
+
+
+    @Test(expected = OrgOutException.class)
+    public void verifyExceptionWhenOrganizationWithThisNameIsNotExists() throws Exception {
+        when(organizationDao.getOrganizationByName("ООО Test123", null, null)).thenThrow(OrganisationValidationException.class);
+        organizationService.getOrganizationByName("ООО Test123", null, null);
     }
 
     @Test
     public void verifyLoadById() throws Exception {
-//        List<OrganizationView> allOrganization = organizationService.getAllOrganization();
-//        allOrganization.get(0);
+
+        when(organizationDao.loadById(1L)).thenReturn(organization);
         OrganizationView view = organizationService.loadById(1L);
-        System.out.println(view.id);
         assertEquals(organization.getName(), view.name);
 
     }
+
+
+    @Test(expected = OrgOutException.class)
+    public void verifyExceptionWhenOrganizationIsNotExists() {
+        when(organizationDao.loadById(100L)).thenThrow(OrganisationValidationException.class);
+        organizationService.loadById(1L);
+    }
+
+
+    @Test
+    public void verifySave() throws Exception {
+        OrganizationViewSave view = new OrganizationViewSave();
+
+        view.name = "ООО Test123";
+        view.fullName = "Общество с ограниченной ответственностью Test123";
+        view.inn = "1233213211";
+        view.kpp = "123321321";
+        view.urAddress = "Покровская";
+        view.phone = "79202222222";
+        view.isActive = true;
+
+        doNothing().when(organizationDao).save(any(Organization.class));
+
+        organizationService.add(view);
+
+        verify(organizationDao).save(any(Organization.class));
+    }
+
+    @Test(expected = OrganisationValidationException.class)
+    public void verifySaveExceptionInn() throws Exception {
+        OrganizationViewSave view = new OrganizationViewSave();
+
+        view.name = "ООО Test123";
+        view.fullName = "Общество с ограниченной ответственностью Test123";
+        view.inn = "12";
+        view.kpp = "123321321";
+        view.urAddress = "Покровская";
+        view.phone = "79202222222";
+        view.isActive = true;
+
+        organizationService.add(view);
+    }
+
+    @Test
+    public void verifyDelete(){
+        Long id = 1L;
+
+        doNothing().when(organizationDao).delete(id);
+
+        organizationService.delete(id);
+
+        verify(organizationDao).delete(id);
+    }
+
+    @Test
+    public void verifyGetAllOrganization() {
+
+        Organization organization1 = new Organization();
+
+        organization1.setName("ООО Test");
+        organization1.setFullName("Общество с ограниченной ответственностью Test");
+        organization1.setInn(1233213211L);
+        organization1.setKpp(123321321L);
+        organization1.setUrAddress("Покровская");
+        organization1.setPhone(79205555555L);
+        organization1.setActive(true);
+
+        Organization organization2 = new Organization();
+        organization2.setName("ООО Test1");
+        organization2.setFullName("Общество с ограниченной ответственностью Test1");
+        organization2.setInn(1233213555L);
+        organization1.setKpp(123852321L);
+        organization1.setUrAddress("Покровская2");
+        organization1.setPhone(79205777755L);
+        organization2.setActive(true);
+
+        List<Organization> organizations = new ArrayList<>();
+        organizations.add(organization1);
+        organizations.add(organization2);
+     //   System.out.println("Лист организаций - " + organizations);
+
+
+        when(organizationDao.getAllOrganization()).thenReturn(organizations);
+        List<OrganizationView> organizationViews = organizationService.getAllOrganization();
+
+
+        assertEquals(organization1.getName(), organizationViews.get(0).name);
+
+    }
+
 }
