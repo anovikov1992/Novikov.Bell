b53c973e0cbb4d7c397983ad7e2827596a06ce8a
anovikov1992 <anovikov1992@yandex.ru>
2018-08-24 10:36:53 +0400

    24.08

--- src/test/java/ru/bellintegrator/practice/organization/service/OrganizationDBTest.java
+++ src/test/java/ru/bellintegrator/practice/organization/service/OrganizationDBTest.java
@@ -4,11 +4,10 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.transaction.annotation.Transactional;
 import ru.bellintegrator.practice.organization.dao.OrganizationDao;
 import ru.bellintegrator.practice.organization.model.Organization;
@@ -17,13 +16,9 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
-@WebAppConfiguration(value = "src/test/resources")
+@SpringBootTest(classes = TestConfig.class)
 @Transactional
 @DirtiesContext
-@AutoConfigureTestDatabase
-
-//@TestPropertySource("classpath:application-test.properties")
 public class OrganizationDBTest {
 
     @Autowired
@@ -32,72 +27,65 @@ public class OrganizationDBTest {
 
     private Organization organization;
 
-    @Before
-    public void setup() {
-        organization = new Organization();
-        organization.setId(100L);
-        organization.setName("ООО Test");
-        organization.setFullName("Общество с ограниченной ответственностью Test");
-        organization.setInn(1236237899L);
-        organization.setKpp(123852897L);
-        organization.setPhone(32165L);
-        organization.setActive(true);
-        organization.setUrAddress("address");
-        organizationDao.save(organization);
-    }
+
+//    @Before
+//    public void setup() {
+//        organization = new Organization();
+//        organization.setId(100L);
+//        organization.setName("ООО Test");
+//        organization.setFullName("Общество с ограниченной ответственностью Test");
+//        organization.setInn(1236237899L);
+//        organization.setKpp(123852897L);
+//        organization.setPhone(32165L);
+//        organization.setActive(true);
+//        organization.setUrAddress("address");
+//        organizationDao.save(organization);
+//    }
 
     @Test
     public void testLoadById() {
-
-        Organization organization = organizationDao.loadById(0L);
+        Organization organization = organizationDao.loadById(3L);
         assertNotNull(organization);
     }
 
 
     @Test
     public void testUpdate() {
-
+        Organization organizationForUpdate = organizationDao.loadById(1L);
         String newName = "ООО Test2";
-        organization.setName(newName);
+        organizationForUpdate.setName(newName);
         String newFullName = "Общество с ограниченной ответственностью Test2";
-        organization.setFullName(newFullName);
+        organizationForUpdate.setFullName(newFullName);
+     //   organizationDao.save(organization);
 
-        organizationDao.save(organization);
-
-        Organization updatedOrganization = organizationDao.loadById(organization.getId());
+        Organization updatedOrganization = organizationDao.loadById(organizationForUpdate.getId());
 
         assertEquals(newName, updatedOrganization.getName());
         assertEquals(newFullName, updatedOrganization.getFullName());
     }
 
 
-    @Test(expected = Exception.class)
+    @Test(expected = EmptyResultDataAccessException.class)
     public void testDelete() {
-
-        Long id = organization.getId();
-
+        Long id = 1L;
         organizationDao.delete(id);
-
         organizationDao.loadById(id);
     }
 
 
-    @Test(expected = Exception.class)
+    @Test(expected = EmptyResultDataAccessException.class)
     public void findNonExistingOrg(){
         Organization nonExistedOrg = new Organization();
         nonExistedOrg.setName("ООО Test1");
         nonExistedOrg.setInn(2224567888L);
         nonExistedOrg.setActive(true);
-
         organizationDao.getOrganizationByName(nonExistedOrg.getName(), null, null);
     }
 
 
     @Test
     public void testFindOrgByName() {
-
-        Organization organizationByName = organizationDao.getOrganizationByName("ООО Test", null, null);
-
-        assertEquals(organizationByName.getName(), organization.getName());
+        Organization organizationByName = organizationDao.getOrganizationByName("Test", 1234567890L, true);
+        assertEquals("Test", organizationByName.getName());
     }
 }
---
+++ src/test/java/ru/bellintegrator/practice/organization/service/TestConfig.java
@@ -0,0 +1,13 @@
+package ru.bellintegrator.practice.organization.service;
+
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import ru.bellintegrator.practice.Application;
+
+@SpringBootConfiguration
+@EnableAutoConfiguration
+@ComponentScan(basePackages = "ru.bellintegrator.practice",
+        excludeFilters = {@ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, value = Application.class)})
+public class TestConfig {}
